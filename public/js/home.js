9ca6933291cf0e582cf461f68a7ba8383105a6da
Si Wai Joanne <ngsiwai@gmail.com>
2016-07-20 10:39:54 +0800

    update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,5 @@
 /* globals $ alert */
+// loads main page with select of cities
 $(document).ready(function () {
   $.ajax({
     url: 'http://localhost:3000/city',
@@ -7,32 +8,20 @@ $(document).ready(function () {
     dataType: 'json',
     success: function (cities) {
       $.each(cities, function (index, item) {
-      $('#add-city').append('<div class="city-box">' + '<h3>' + cities[index].name + '</h3>' + '</div>' + '</br>')
-    })
-  },
+        $('#add-city').append('<div class="city-box">' + '<h3>' + cities[index].name + '</h3>' + '</div>' + '</br>')
+      })
+    },
     error: function (xhr, status) {
       alert('Error: Unable to load.')
     }
   })
+  // when city button is clicked, remove attractions from view
   $('#city-btn').on('click', function (event) {
     $('#add-att').empty()
-    $('#add-city').empty()
     $('#attract').empty()
-    $.ajax({
-      url: 'http://localhost:3000/city',
-      type: 'GET',
-      crossDomain: true,
-      dataType: 'json',
-      success: function (cities) {
-        $.each(cities, function (index, item) {
-        $('#add-city').append('<div class="city-box">' + '<h3>' + cities[index].name + '</h3>' + '</div>' + '</br>')
-        })
-      },
-      error: function (xhr, status) {
-        alert('Error: Unable to load.')
-      }
-    })
+    $('#add-city').show()
   })
+  // when use selects a city, all city's attractions will be shown. The text is from the city-box div which has the city's name. The city's name is then included in the url to direct to the corresponding city and it's attractions
   $(document).on('click', '.city-box', function (event) {
     var text = $(this).text()
     $.ajax({
@@ -55,6 +44,7 @@ $(document).ready(function () {
       }
     })
   })
+  // Attractions will be pushed into the empty array on each click.
   var attractArray = []
   $(document).on('click', '.att-name', function (event) {
     for (var i = 0; i < $(this).length; i++) {
@@ -64,12 +54,11 @@ $(document).ready(function () {
     console.log(attractArray)
   })
   $(document).on('click', '#trip-plan', function (event) {
-    $('#add-city').empty()
+    $('#add-city').hide()
     $('#attract').empty()
     $('#add-att').show()
     $.each(attractArray, function (index, item) {
       $('#add-att').append('<div class="att-box">' + '<h3>' + attractArray[index] + '</h3>' + '</div>' + '</br>')
-      // window.localStorage.trip = attractArray
     })
   })
   $(document).on('click', '#save-trip', function (event) {
