5afb3bc100f69e50a353f83267208da36355721a
Dominic <jxdlam@gmail.com>
2016-07-21 16:15:33 +0800

    changed user_auth.js to check faster

--- public/js/user_auth.js
+++ public/js/user_auth.js
@@ -3,8 +3,8 @@ var serverURL = 'http://localhost:3000/'
 var currentUser = null
 
 // We are assuming Local Storage is supported
+if (!window.localStorage['email'] || !window.localStorage['auth_token']) window.location.href = 'login.html'
 $(function () {
-  if (!window.localStorage['email'] || !window.localStorage['auth_token']) window.location.href = 'login.html'
   // else loadUser()
 })
 
